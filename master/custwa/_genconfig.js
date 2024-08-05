1f75dcd5c171000910cc50abe3cc959cd836d45b
agung-transfashion <agung@transfashionindonesia.com>
2024-08-05 08:42:48 +0700

    add custwa

---
+++ master/custwa/_genconfig.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const dbtype = global.dbtype;
+const comp = global.comp;
+
+module.exports = {
+	title: "Customer Whatsapp",
+	autoid: true,
+
+	persistent: {
+		'mst_custwa' : {
+			primarykeys: ['custwa_id'],
+			comment: 'Daftar Customer Whatsapp',
+			data: {
+				custwa_id: {text:'ID', type: dbtype.varchar(16), null:false},
+				custwa_name: {text:'Name', type: dbtype.varchar(60), null:false, options:{required:true,invalidMessage:'Name harus diisi'}},
+				custwa_gender: {text:'Gender', type: dbtype.varchar(1), null:true},
+			},
+
+			defaultsearch : ['custwa_id', 'custwa_name']
+
+		},
+
+		'mst_custwalinkreq' : {
+			primarykeys: ['custwalinkreq_id'],
+			comment: 'Daftar Contact Customer',
+			data: {
+				custwalinkreq_id: {text:'ID', type: dbtype.varchar(14), null:false},
+				intent: {text:'Intent', type: dbtype.varchar(64), null:false},
+				message: {text:'Message', type: dbtype.varchar(1024), null:false},
+				data: {text:'Data', type: dbtype.varchar(2048), null:false},
+				custwa_id: {text:'Customer', type: dbtype.varchar(14), null:false, hidden: true},
+			}
+		}
+	},
+
+	schema: {
+		title: 'Customer WA',
+		header: 'mst_custwa',
+		detils: {
+			'link' : {title: 'Link', table:'mst_custwalinkreq', form: true, headerview:'custwa_name', editorHandler: true, listHandler: true},
+		}
+	}
+}
---
+++ master/custwa/custwa.sql
@@ -0,0 +1,72 @@
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- drop table if exists `mst_custwa`;
+-- drop table if exists `mst_custwalinkreq`;
+
+
+CREATE TABLE IF NOT EXISTS `mst_custwa` (
+	`custwa_id` varchar(16) NOT NULL , 
+	`custwa_name` varchar(60) NOT NULL , 
+	`custwa_gender` varchar(1)  , 
+	`_createby` varchar(14) NOT NULL , 
+	`_createdate` datetime NOT NULL DEFAULT current_timestamp(), 
+	`_modifyby` varchar(14)  , 
+	`_modifydate` datetime  , 
+	PRIMARY KEY (`custwa_id`)
+) 
+ENGINE=InnoDB
+COMMENT='Daftar Customer Whatsapp';
+
+
+ALTER TABLE `mst_custwa` ADD COLUMN IF NOT EXISTS  `custwa_name` varchar(60) NOT NULL  AFTER `custwa_id`;
+ALTER TABLE `mst_custwa` ADD COLUMN IF NOT EXISTS  `custwa_gender` varchar(1)   AFTER `custwa_name`;
+
+
+ALTER TABLE `mst_custwa` MODIFY COLUMN IF EXISTS  `custwa_name` varchar(60) NOT NULL   AFTER `custwa_id`;
+ALTER TABLE `mst_custwa` MODIFY COLUMN IF EXISTS  `custwa_gender` varchar(1)    AFTER `custwa_name`;
+
+
+
+
+
+
+
+
+
+CREATE TABLE IF NOT EXISTS `mst_custwalinkreq` (
+	`custwalinkreq_id` varchar(14) NOT NULL , 
+	`intent` varchar(64) NOT NULL , 
+	`message` varchar(1024) NOT NULL , 
+	`data` varchar(2048) NOT NULL , 
+	`custwa_id` varchar(14) NOT NULL , 
+	`_createby` varchar(14) NOT NULL , 
+	`_createdate` datetime NOT NULL DEFAULT current_timestamp(), 
+	`_modifyby` varchar(14)  , 
+	`_modifydate` datetime  , 
+	PRIMARY KEY (`custwalinkreq_id`)
+) 
+ENGINE=InnoDB
+COMMENT='Daftar Contact Customer';
+
+
+ALTER TABLE `mst_custwalinkreq` ADD COLUMN IF NOT EXISTS  `intent` varchar(64) NOT NULL  AFTER `custwalinkreq_id`;
+ALTER TABLE `mst_custwalinkreq` ADD COLUMN IF NOT EXISTS  `message` varchar(1024) NOT NULL  AFTER `intent`;
+ALTER TABLE `mst_custwalinkreq` ADD COLUMN IF NOT EXISTS  `data` varchar(2048) NOT NULL  AFTER `message`;
+ALTER TABLE `mst_custwalinkreq` ADD COLUMN IF NOT EXISTS  `custwa_id` varchar(14) NOT NULL  AFTER `data`;
+
+
+ALTER TABLE `mst_custwalinkreq` MODIFY COLUMN IF EXISTS  `intent` varchar(64) NOT NULL   AFTER `custwalinkreq_id`;
+ALTER TABLE `mst_custwalinkreq` MODIFY COLUMN IF EXISTS  `message` varchar(1024) NOT NULL   AFTER `intent`;
+ALTER TABLE `mst_custwalinkreq` MODIFY COLUMN IF EXISTS  `data` varchar(2048) NOT NULL   AFTER `message`;
+ALTER TABLE `mst_custwalinkreq` MODIFY COLUMN IF EXISTS  `custwa_id` varchar(14) NOT NULL   AFTER `data`;
+
+
+
+ALTER TABLE `mst_custwalinkreq` ADD KEY IF NOT EXISTS `custwa_id` (`custwa_id`);
+
+ALTER TABLE `mst_custwalinkreq` ADD CONSTRAINT `fk_mst_custwalinkreq_mst_custwa` FOREIGN KEY IF NOT EXISTS (`custwa_id`) REFERENCES `mst_custwa` (`custwa_id`);
+
+
+
+
+
